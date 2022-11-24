56ebde1da546e264d898b83c15485a8e0375b8f5
HARSHIT SAINI <hsaini@mathworks.com>
2022-11-24 19:01:37 +0530

    edit form

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal, Button, Card, Container, Row } from 'react-bootstrap';
 import GameCard from './GameCard';
-import { games } from '../data/data';
 
 const Home = () => {
 	const [show, setShow] = useState(false);
@@ -28,20 +27,7 @@ const Home = () => {
 					<Modal.Title>{formGameType}</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<Card className='text-center'>
-						<Card.Header>Featured</Card.Header>
-						<Card.Body>
-							<Card.Title>Special title treatment</Card.Title>
-							<Card.Text>
-								With supporting text below as a natural lead-in
-								to additional content.
-							</Card.Text>
-							<Button variant='primary'>Go somewhere</Button>
-						</Card.Body>
-						<Card.Footer className='text-muted'>
-							2 days ago
-						</Card.Footer>
-					</Card>
+					<GameForm />
 				</Modal.Body>
 				<Modal.Footer>
 					<Button variant='secondary' onClick={handleClose}>
