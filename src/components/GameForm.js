1916096078af90add7c5f3e6f50f067310eb54b7
Mili Goyal <mgoyal@vdi-dd1bgl-029.dhcp.mathworks.com>
2022-11-25 14:30:14 +0530

    close Modal added

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -65,6 +65,7 @@ function GameForm(props) {
 		e.preventDefault();
 		// alert('assume I just submitted the form');
 		console.log('submittedForm', equipments, day, timeSlot, headCount);
+		props.onClick();
 		Swal.fire({
 			title: 'Success!',
 			text: 'Your slot has been booked. You will receive an email regarding it!',
